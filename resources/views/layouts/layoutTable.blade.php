105f5d3bbd870328375f70e2f8747c0eccfe30f2
Dane Alcazar <58464386+IamNotDane@users.noreply.github.com>
2022-02-07 01:07:56 +0800

    eto na nga

--- resources/views/layouts/layoutTable.blade.php
+++ resources/views/layouts/layoutTable.blade.php
@@ -78,6 +78,9 @@
                     <a class="dropdown-item" href="{{ route('profile.index') }}">My profile</a>
                   </li>
                   <li>
+                    <a class="dropdown-item" href="/">Home</a>
+					        </li>
+                  <li>
                     <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                   document.getElementById('logout-form').submit();">
