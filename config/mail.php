fba35ed5444d254c2ac47a52ca87333b1abb6c69
Dane Alcazar <58464386+IamNotDane@users.noreply.github.com>
2022-02-07 02:18:00 +0800

    eto na nga kasi ulit yon

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
